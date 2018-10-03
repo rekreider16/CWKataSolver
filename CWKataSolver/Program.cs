2099892066f972fdea88398fc30ed6570d190e85
Evan <rekreider@una.edu>
2018-10-03 12:57:31 -0500

    Add project files.

---
+++ CWKataSolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CWKataSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
